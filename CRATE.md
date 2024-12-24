aaa9bcf495070c11d3c26070f5bbcb29941d451d
Vinicius Gobbo <vinicius.gobbo@booking.com>
2024-12-24 16:40:14 +0100

    Crate documentation.

---
+++ CRATE.md
@@ -0,0 +1,11 @@
+# Duration Flex
+
+Helper class to make it easier to specify duration files. Specially useful in configuration files.
+
+**Example:**
+- 1 hour and 23 minutes: `1h23m`
+- 1 week, 6 days, 23 hours, 49 minutes andd 50 seconds: `1w6d23h49m59s`
+
+## Features
+- `clap`: enable clap support, so it can be used as application arguments.
+- `serde`: enable serde support.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+/// # Duration Flex
+///
+/// Helper class to make it easier to specify duration files. Specially useful in configuration files.
+///
+/// **Example:**
+/// - 1 hour and 23 minutes: `1h23m`
+/// - 1 week, 6 days, 23 hours, 49 minutes andd 50 seconds: `1w6d23h49m59s`
+///
+/// ## Features
+/// - `clap`: enable clap support, so it can be used as application arguments.
+/// - `serde`: enable serde support.
 use std::fmt::{Display, Formatter};
 use std::ops::{Add, Sub};
 use std::str::FromStr;
